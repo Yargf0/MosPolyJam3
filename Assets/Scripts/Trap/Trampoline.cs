95a822a68a21905412efee7688cc14219d4234cf
Yar <Yar55rus@gmail.com>
2024-11-11 18:44:24 +0300

    BigCommit

--- Assets/Scripts/Trap/Trampoline.cs
+++ Assets/Scripts/Trap/Trampoline.cs
@@ -5,6 +5,8 @@ public class Trampoline : InvertableBehaviour
     [Header("Trampoline Settings")]
     public float verticalForce = 10f;
     public float sideForce = 5f;
+    [SerializeField] private Animator animator;
+    private readonly int AnimationHash = Animator.StringToHash("activated");
 
 
     private void OnTriggerEnter(Collider other)
@@ -17,7 +19,7 @@ public class Trampoline : InvertableBehaviour
 
     private void LaunchPlayer()
     {
-        Debug.Log("thth");
+        animator.Play(AnimationHash);
         Vector3 launchVelocity = Vector3.up * verticalForce;
 
         if (isInverted)
